801c685fe16b0ec509b72f8341e1bac261b46955
KatoCode23 <Kalen23Imani@gmail.com>
2017-11-19 08:07:19 -0500

    added a style sheet

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/main.css';
+
+const slideShowImages = [
+{image: 'space1.jpg'},
+{image: 'space2.jpg'},
+{image: 'space3.jpg'},
+{image: 'space4.jpg'},
+{image: 'space5.jpg'},
+];
+
+console.log(slideShowImages);
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(
+  <h1>Hello, World!</h1>,
+  document.getElementById('root')
+);
